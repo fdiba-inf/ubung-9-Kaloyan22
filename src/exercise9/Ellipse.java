a2e4297bfbf99f17e13e3b5b51dfd73a3db31cf1
Kaloyan22 <91698321+Kaloyan22@users.noreply.github.com>
2021-12-20 16:10:42 +0000

    .

--- src/exercise9/Ellipse.java
+++ src/exercise9/Ellipse.java
@@ -1,4 +1,68 @@
 package exercise9;
 
 public class Ellipse {
+    private Point startPoint;
+    private double a;
+    private double b;
+
+    public Ellipse() {
+        startPoint = new Point(0, 0);
+        a = 1;
+        b = 1;
+    }
+
+    public Ellipse(Point startPoint, double a, double b) {
+        this.startPoint = new Point(startPoint);
+        this.a = a;
+        this.b = b;
+    }
+
+    public Ellipse(Ellipse ellipseVar) {
+        startPoint = new Point(ellipseVar.startPoint);
+        a = ellipseVar.a;
+        b = ellipseVar.b;
+    }
+
+    public String Shape() {
+      if(a==b) {
+        return "Circle";
+      }
+      else {
+        return "Ellipse";
+      }
+    }
+
+    public double P() {
+        return Math.PI*(3*(a + b) - Math.sqrt((3*a+b)*(a+3*b)));
+    }
+    
+    public boolean Validation() {
+        return a > 0 && b > 0;
+    }
+    public double S() {
+        return a * b * Math.PI;
+    }
+    public String ConvertToStr() {
+        return String.format("%s-[%s, %s], %s, P=%s, A=%s", startPoint, a, b, Shape(), P(), S());
+    }
+
+    public boolean equal(Ellipse ellipseVar) {
+        boolean A2 = Utils.equals(a, ellipseVar.a);
+        boolean B2 = Utils.equals(b, ellipseVar.b);
+        boolean A2Inverted = Utils.equals(a, ellipseVar.a);
+        boolean B2Inverted = Utils.equals(b, ellipseVar.b);
+
+        return (A2 && B2) || (A2Inverted && B2Inverted);
+    }
+
+    public void initialize() {
+        do {
+            System.out.println("Start point: ");
+            startPoint.initialize();
+            System.out.print("a: ");
+            a = Utils.INPUT.nextDouble();
+            System.out.print("b: ");
+            b = Utils.INPUT.nextDouble();
+        } while (!Validation());
+    }
 }
--- src/exercise9/EllipseDemo.java
+++ src/exercise9/EllipseDemo.java
@@ -2,6 +2,20 @@ package exercise9;
 
 public class EllipseDemo {
     public static void main(String[] args) {
+        Point startPoint2 = new Point(2, 3);
 
+        Ellipse FirstEllipse = new Ellipse();
+        Ellipse SecondEllipse = new Ellipse(startPoint2, 5, 6);
+        Ellipse ThirdEllipse = new Ellipse(SecondEllipse);
+        Ellipse FourthEllipse = new Ellipse();
+        
+        FourthEllipse.initialize();
+
+        System.out.println("Ellipse 1: " + FirstEllipse);
+        System.out.println("Ellipse 2: " + SecondEllipse);
+        System.out.println("Ellipse 3: " + ThirdEllipse);
+        System.out.println("Ellipse 4: " + FourthEllipse);
+        System.out.println("Ellipse 2 equals Ellipse 1: " + SecondEllipse.equal(FirstEllipse));
+        System.out.println("Ellipse 2 equals Ellipse 3: " + SecondEllipse.equal(ThirdEllipse));
     }
 }
